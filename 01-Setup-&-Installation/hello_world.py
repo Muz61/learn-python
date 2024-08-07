ff12bd227f15230c229342880e04ab76121a64d8
Muz61 <ciaranjmurray314@gmail.com>
2024-08-07 07:26:41 +0000

    commit hello world

--- 01-Setup-&-Installation/hello_world.py
+++ 01-Setup-&-Installation/hello_world.py
@@ -1,6 +1,6 @@
-print("hello world!")
-print("this is me!")
-print("life should be!")
+print("hello world")
+print("this is me")
+print("life should be")
 print("fun for everyone!")
 
 # this is a comment
