923a2ba39991bef8428ecfa43f1595fa28abf41c
Muz61 <ciaranjmurray314@gmail.com>
2024-08-28 06:50:21 +0000

    print function III

---
+++ 02-Welcome-to-Python/the-print-function-III-parameters-and-arguments.py
@@ -0,0 +1,13 @@
+print("ABC","DEF","XYZ")
+print("ABC","DEF","XYZ", sep="!")
+print("ABC","DEF","XYZ", sep="--*--")
+print("ABC","DEF","XYZ", sep=" ")
+
+print("ABC","DEF","XYZ", "!")
+
+print("hello", "goodbye", end="!&*")
+print("hello")
+
+
+print("A", "B","C", sep="**", end="#")
+print("A", "B","C", end="#", sep="**")
