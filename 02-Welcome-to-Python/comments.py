6588cf9e489ec58fc90242c79baccd00047099db
Muz61 <ciaranjmurray314@gmail.com>
2024-08-28 07:06:08 +0000

    Comments

---
+++ 02-Welcome-to-Python/comments.py
@@ -0,0 +1,11 @@
+print("Good Morning")
+#print("Good Afternoon")
+print("Good Night")
+
+
+#This will perform a Mathematical formula
+print(1 + 1) #Adds 1 and 1
+
+# print("hello world")
+# print("this is me")
+# print("life should be")
