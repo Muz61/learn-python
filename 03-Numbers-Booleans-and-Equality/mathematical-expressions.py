eb418e678eab89b248efd6e8814b070ff8c67db0
Muz61 <ciaranjmurray314@gmail.com>
2024-09-11 06:28:27 +0000

    Mathematical expressions

---
+++ 03-Numbers-Booleans-and-Equality/mathematical-expressions.py
@@ -0,0 +1,15 @@
+print(3 + 4)
+print(3.5 + 6.3)
+print(10 - 5)
+print(5 * 3)
+
+print("5" + "3")
+print("lo" * 11 + "l")
+
+print(10 + 3.8)
+
+print(5 ** 3)
+print(4 **4)
+
+print(3 + 5 * 3)
+print((3 + 5) * 3)
