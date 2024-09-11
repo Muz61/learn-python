33942656d84f559bb3ee6794153fcda4df4e404c
Muz61 <ciaranjmurray314@gmail.com>
2024-09-11 06:57:59 +0000

    George Bool

---
+++ 03-Numbers-Booleans-and-Equality/boolean-mathematical-operators.py
@@ -0,0 +1,32 @@
+##George Bool
+
+print(True)
+print(False)
+print("True")
+print("False")
+
+print(5 == 5)
+print(5 == 8)
+print(8.3 == 8.3)
+print(8.3 == 4.1)
+print(5 == "5")
+
+print("ham" == "ham")
+print("ham" == "Ham")
+print("" == "")
+print("!==*" == "!==*")
+
+print(5 == 5.0)
+print(5 == 5.1)
+
+print(True == True)
+print(False == False)
+print(True == False)
+
+print(5 != 6)
+print(10 != 10)
+print("music" != "music")
+print("The Beatles" != "Music", "lol")
+
+print(10 != "10")
+print(10.0 != 10)
